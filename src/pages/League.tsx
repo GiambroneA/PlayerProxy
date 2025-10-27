46f95657707ef98baaa84e34b1805155c152f7ad
FilipCvet <filip.cvetkovski45@gmail.com>
2025-10-27 16:48:20 -0400

    added code to league page

---
+++ src/pages/League.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import logo from "../assets/logo.png";
+
+const Home: React.FC = () => {
+  return (
+    <div className="relative min-h-screen bg-black text-white flex flex-col items-center justify-center">
+      <Navbar />
+
+      {/* Centered content */}
+      <div className="flex flex-col items-center justify-center mt-24 text-center">
+        <img
+          src={logo}
+          alt="PlayerProxy Logo"
+          className="w-56 h-56 mb-6 drop-shadow-2xl"
+        />
+        <h1 className="text-5xl font-bold mb-4 tracking-wide">
+          League
+        </h1>
+        <p className="text-lg text-gray-300">
+            league information will be here.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
